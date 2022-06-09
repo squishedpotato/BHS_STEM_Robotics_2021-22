f80b53055c5126b8ff9f2a25cf962759d30800af
squishedpotato <76015998+squishedpotato@users.noreply.github.com>
2022-06-09 09:31:02 -0400

    Update TelemetryDataHelper.java

--- TelemetryDataHelper.java
+++ TelemetryDataHelper.java
@@ -29,20 +29,34 @@ public class TelemetryDataHelper {
         telemetry.addData("backright pow", backright.getPower());
         telemetry.addData("backright encoder", backright.getCurrentPosition());
     }
+    public void telemetryDisplayMotor(DcMotor motor, String motorName){
+        telemetry.addData(motorName, motor.getPower());
+        telemetry.addData(motorName, motor.getCurrentPosition);
+    }
+    /* Preconditions: motor and motorName arrays are the same length, where each motor maps to a motorName
+     * @param motor An array of DcMotors to be displayed as data
+     * @param motorName An array of Strings that name each DcMotor in the array
+     * @throws IllegalArgumentException if motor.length != motorName.length
+     */
+    public void telemetryDisplayMotorArray(DcMotor[] motor, String[] motorName){
+        for(int i = 0; i < motor.length; i++){
+            telemetryDisplayMotor(motor[i], motorName[i]);
+        }
+    }
     public void telemetryDisplayControllerG1(){
-        telemetry.addData("GP1 Left Stick X: ", gamepad1.left_stick_x);
-        telemetry.addData("GP1 Left Stick Y: ", gamepad1.left_stick_y);
-        telemetry.addData("GP1 Right Stick X: ", gamepad1.right_stick_x);
-        telemetry.addData("GP1 Right Stick Y: ", gamepad1.right_stick_y);
-        telemetry.addData("GP1 Left Trigger:", gamepad1.left_trigger);
-        telemetry.addData("GP1 Right Trigger:", gamepad1.right_trigger);
+        telemetry.addData("GP1 Left Stick X ", gamepad1.left_stick_x);
+        telemetry.addData("GP1 Left Stick Y ", gamepad1.left_stick_y);
+        telemetry.addData("GP1 Right Stick X ", gamepad1.right_stick_x);
+        telemetry.addData("GP1 Right Stick Y ", gamepad1.right_stick_y);
+        telemetry.addData("GP1 Left Trigger ", gamepad1.left_trigger);
+        telemetry.addData("GP1 Right Trigger ", gamepad1.right_trigger);
     }
     public void telemetryDisplayControllerG2(){
-        telemetry.addData("GP2 Left Stick X: ", gamepad2.left_stick_x);
-        telemetry.addData("GP2 Left Stick Y: ", gamepad2.left_stick_y);
-        telemetry.addData("GP2 Right Stick X: ", gamepad2.right_stick_x);
-        telemetry.addData("GP2 Right Stick Y: ", gamepad2.right_stick_y);
-        telemetry.addData("GP2 Left Trigger:", gamepad2.left_trigger);
-        telemetry.addData("GP2 Right Trigger:", gamepad2.right_trigger);
+        telemetry.addData("GP2 Left Stick X ", gamepad2.left_stick_x);
+        telemetry.addData("GP2 Left Stick Y ", gamepad2.left_stick_y);
+        telemetry.addData("GP2 Right Stick X ", gamepad2.right_stick_x);
+        telemetry.addData("GP2 Right Stick Y ", gamepad2.right_stick_y);
+        telemetry.addData("GP2 Left Trigger ", gamepad2.left_trigger);
+        telemetry.addData("GP2 Right Trigger ", gamepad2.right_trigger);
     }
 }
